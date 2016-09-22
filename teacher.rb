dba6a20bb857769a135146fa88a488ae603ea12c
Robbie Santos <robbiealixsantos@gmail.com>
2016-09-22 10:02:46 -0700

    Adds kind of completed checkpoint challenge 3

--- teacher.rb
+++ teacher.rb
@@ -1,22 +1,10 @@
-class Teacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  attr_accessor :name
+require_relative 'allteachers'
 
-  def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
-    @target_raise = 1000
-  end
+class Teacher < AllTeachers
+  attr_reader :salary, :performance_rating, :target_raise
 
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
+  TARGET_RAISE = 1000
+  PHASE = 3
 
   def teach_stuff
     response = ""
@@ -26,19 +14,11 @@ class Teacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
 
   def set_performance_rating(rating)
     response = ""
     if rating > 90
-      receive_raise(@target_raise)
+      receive_raise(TARGET_RAISE)
       response = "Yay, I'm a great employee!"
     else
       response += "Oh, well -- thanks to this actionable, specific, and kind "
