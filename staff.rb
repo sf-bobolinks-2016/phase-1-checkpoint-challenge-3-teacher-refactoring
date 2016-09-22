29f853145123cfa90728f4bfa9103a104fe2c1b0
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-22 09:58:37 -0700

    Remove unnecessary call to assignment operator in String concatenation.

--- staff.rb
+++ staff.rb
@@ -28,8 +28,7 @@ class Staff < Person
       receive_raise(@target_raise)
       response = "Yay, I'm a great employee!"
     else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
+      response += "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
     end
     response
   end
