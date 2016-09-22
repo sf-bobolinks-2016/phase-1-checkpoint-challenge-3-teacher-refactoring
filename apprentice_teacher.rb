2a8a1f4baf1eaae752fcc51c3c3df84852aa361d
Jason Warholic <warholic.jason@gmail.com>
2016-09-22 09:43:06 -0700

    Refactor to include school class and teaching module

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,4 +1,8 @@
-class ApprenticeTeacher
+require_relative 'school'
+require_relative 'Teaching'
+
+class ApprenticeTeacher < School
+  include Teaching
   attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
@@ -9,30 +13,12 @@ class ApprenticeTeacher
     @phase = 3
   end
 
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
-
   def teach_stuff
     response = ""
     response += "Listen, class, this is how everything works. "
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
     response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
