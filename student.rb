d6e9e268b927679138e27ca9d90328c91c0c4567
DL804 <darren804@gmail.com>
2016-09-22 09:48:42 -0700

    create module and new class, tests good

--- student.rb
+++ student.rb
@@ -1,16 +1,16 @@
-class Student
+require_relative 'tool'
+
+class Student 
+include Tool
+
   attr_reader :age, :phase
   attr_accessor :name
 
   def initialize(options = {})
     @phase = 1
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+ 
   end
 
-  def offer_high_five
-    "High five!"
-  end
 
   def set_phase(num)
     response = ""
