65b24547a8042f503585ffa1e3ec289b804f4e45
marvan_t <marvan_t@hotmail.com>
2016-09-22 09:44:21 -0700

    create parent class for teacher and apprentice, & create school class to be a parent class for students and teahcers

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,6 @@
-class Teacher
+require_relative 'school_teachers'
+
+class Teacher < SchoolTeachers
   attr_reader :age, :salary, :phase, :performance_rating, :target_raise
   attr_accessor :name
 
@@ -13,10 +15,10 @@ class Teacher
     "High five!"
   end
 
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
+  # def set_phase(num)
+  #   @phase = num
+  #   "Cool, I've always wanted to teach phase #{num}!"
+  # end
 
   def teach_stuff
     response = ""
@@ -26,14 +28,14 @@ class Teacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
+  # def salary=(new_salary)
+  #   puts "This better be good!"
+  #   @salary = new_salary
+  # end
 
-  def receive_raise(raise)
-    @salary += raise
-  end
+  # def receive_raise(raise)
+  #   @salary += raise
+  # end
 
   def set_performance_rating(rating)
     response = ""
