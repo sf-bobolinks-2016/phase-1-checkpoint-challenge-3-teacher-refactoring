32f247e615c21b865fe0fbdcb62622b360b04f64
Jaime Rios <jaimejrios1@gmail.com>
2016-09-22 09:30:48 -0700

    dry out ApprenticeTeacher and Teacher classes by removing unnecessary methods and instance variables

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,5 @@
 class Teacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
+  attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
   def initialize(options={})
