88bda407655a2c9b24d11f0ad4d18ef079175559
King Heng Jonathan Lok <email": null>
2016-09-22 10:04:16 -0700

    Add a high five module and change apprentice teacher to inheritance teacher

--- teacher.rb
+++ teacher.rb
@@ -1,17 +1,24 @@
+require_relative 'high_five'
+
 class Teacher
+
+  include HighFive
+
+  TARGET_RAISE = 1000
+  PHASE = 3
   attr_reader :age, :salary, :phase, :performance_rating, :target_raise
   attr_accessor :name
 
   def initialize(options={})
-    @phase = 3
+    @phase = self.class::PHASE
     @age = options.fetch(:age, 0)
     @name = options.fetch(:name, "")
-    @target_raise = 1000
+    @target_raise = self.class::TARGET_RAISE
   end
 
-  def offer_high_five
-    "High five!"
-  end
+  # def offer_high_five
+  #   "High five!"
+  # end
 
   def set_phase(num)
     @phase = num
