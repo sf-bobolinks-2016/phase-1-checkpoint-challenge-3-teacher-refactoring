59169b02465814c34aa12b3aa1c3578dec17c007
Nestor J. Alvarez <grimreaper1169@gmail.com>
2016-09-22 09:43:32 -0700

    Refactor code based on repeated methods. Methods that need overrriding not supplied to parent class.

--- person.rb
+++ person.rb
@@ -2,7 +2,9 @@ class Person
 
   attr_reader :age, :phase
   attr_accessor :name
-
   
-
+  def offer_high_five
+    return "High five!"
+  end
+  
 end
--- staff.rb
+++ staff.rb
@@ -4,4 +4,18 @@ class Staff < Person
 
   attr_reader :salary, :target_raise
 
+  def set_phase(num)
+    @phase = num
+    "Cool, I've always wanted to teach phase #{num}!"
+  end
+
+  def salary=(new_salary)
+    puts "This better be good!"
+    @salary = new_salary
+  end
+
+  def receive_raise(raise)
+    @salary += raise
+  end
+
 end
